c7b389b5ba7f31327c8a804406a63a222c55dec6
Yuliia Kharchenko <jkharchenko2008@gmail.com>
2021-08-26 11:16:21 +0300

    создаёт базовые файлы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './sass/main.scss';
+// import fetchImage from './apiService';
+import getRefs from './refs';
+
+// console.log(fetchImage());
+const refs = getRefs;
+console.log(refs.container);
+
+// renderForm();
+
+// function renderForm() {
+//   const form = `<form class="search-form" id="search-form">
+//       <input type="text" name="query" autocomplete="off" placeholder="Search images..." />
+//     </form>`;
+
+//   refs.container.insertAdjacentHTML('beforeend', form);
+// }
---
+++ src/refs.js
@@ -0,0 +1,5 @@
+export default function getRefs() {
+  return {
+    container: document.querySelector('.container'),
+  };
+}
